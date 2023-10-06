2e43976b1c72375a1acb7e33ca582547a38219e2
ismylravafidh <bd82v6x9y@code.edu.az>
2023-10-06 11:17:20 +0400

    Task Ref_Out

---
+++ Task1.cs
@@ -0,0 +1,37 @@
+﻿//Ədədlərdən ibarət arrayın ən kiçik və ən böyük elementinin yerini dəyişən method yazın.
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayMinToMax(2, 3, 1, 4, 5, 19, 6, 22, 10);
+        }
+        static void ArrayMinToMax(params  int[] array)
+        {
+            int MinValue = array[0];
+            int MaxValue = array[0];
+            int MinValueIndex = 0;
+            int MaxValueIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < MinValue)
+                {
+                    MinValue = array[i];
+                    MinValueIndex = i;
+                }
+                if (array[i] > MaxValue)
+                {
+                    MaxValue = array[i];
+                    MaxValueIndex = i;
+                }
+            }
+            for (int i = 0;i < array.Length; i++)
+            {
+                array[MinValueIndex] = MaxValue;
+                array[MaxValueIndex] = MinValue;
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,31 @@
+﻿//InsertArray methodu yaradılır. Method parametr olaraq int tipindən array və params olaraq int-lər qəbul edir.
+// Paramslardan gələn dəyərləri həmin arraya əlavə etsin və sonda dəyişilmiş hal köhnə arraya bərabər olsun
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1,2,3,4 };
+            InsertArray(5,6,ref array);
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void InsertArray(int num1 , int num2 ,ref int[] array)
+        {
+            int[] copy = new int[array.Length+2];
+            for (int i = 0; i < array.Length; i++)
+            {
+                copy[i] = array[i];
+            }
+            if (copy[array.Length] == default(int) && copy[array.Length + 1] == default(int))
+            {
+                copy[array.Length] = num1;
+                copy[array.Length+1] = num2;
+            }
+            array = copy;
+        }
+    }
+}
